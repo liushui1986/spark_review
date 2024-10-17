206e89b5ad18e2d403d3c9a287eb5026662d8068
Shuaipeng Ma <160495217+liushui1986@users.noreply.github.com>
2024-10-16 22:31:01 -0500

    find max temperature of station

---
+++ max-temperatures_sm.py
@@ -0,0 +1,67 @@
+# This PySpark script processes a CSV file containing temperature data from various weather stations.
+
+# Import PySpark libraries
+# SparkConf is used to configure the Spark application
+# SparkContext represents the connnection to the Spark cluster and is used to initialize RDDs
+from pyspark import SparkConf, SparkContext
+
+# Configure Spark
+# setMaster("local") means running Spark job locally (on one machine)
+# setAppName means naming the application for identification in logs or UI
+# SparkContext creates a Spark context, which is the entry point to using Spark's features.
+conf = SparkConf().setMaster("local").setAppName("MaxTemperatures")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    """
+    the function takes a line from the CSV file, splits it by commas, and extracts stationID,
+    entryType and temperature
+    Parameters
+    ----------
+    line : str
+        a string from the csv file, splitted by commas.
+
+    Returns
+    -------
+    stationID : str
+        station ID.
+    entryType : str
+        min or max or others.
+    temperature : str
+        temperature.
+
+    """
+    fields = line.split(',')
+    stationID = fields[0]
+    entryType = fields[2]
+    
+    # fields[3] contains number in tenths of degrees Celsius.
+    temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0  
+    return (stationID, entryType, temperature)
+
+# Load data and parse lines
+# Loads the CSV file into an RDD, with each line as a string
+lines = sc.textFile("file:///Users/mashu/Documents/study/spark/1800.csv")
+
+# Applies the parseline function to each line, creating a new RDD where each record is a tuple
+parsedLines = lines.map(parseLine)
+
+# Filter for minimum temperatures
+# Filters the RDD to keep only records where the entryType is "TMAX".
+maxTemps = parsedLines.filter(lambda x: "TMAX" in x[1])
+
+# MAP to (stationID, temperature) pairs
+# Transforms the RDD to only include the station ID and temperature, ignoring the entryType (all "TMAX")
+stationTemps = maxTemps.map(lambda x: (x[0], x[2]))
+
+# Find the minimum temperature per Station
+# Aggregates the temperatures by station ID, keeping only the maximum temperature for each station.
+# It compares two temperature values x and y and returns the lower one.
+maxTemps = stationTemps.reduceByKey(lambda x, y: max(x,y))
+
+# Collect the results
+# Brings the results from all Spark workers to the driver node(local machine), returning a list of pairs
+results = maxTemps.collect();
+
+for result in results:
+    print(result[0] + "\t{:.2f}F".format(result[1]))
